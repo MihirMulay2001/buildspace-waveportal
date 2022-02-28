85eb584f337f980b233ed92e1570b926e0437cf1
Mihir Mulay <55177881+MihirMulay2001@users.noreply.github.com>
2022-03-01 02:21:29 +0530

    linked to contract on rinkeby network

--- src/App.js
+++ src/App.js
@@ -1,28 +1,104 @@
 import * as React from "react";
 import { ethers } from "ethers";
 import './App.css';
+import abi from './utils/WavePortal.json'
 
 export default function App() {
 
-  const wave = () => {
+  const wave = async () => {
+     try {
+      const { ethereum } = window;
+      const contractAddress = "0x053F5A2E2b66a213d5f22c3Cf578E31d501AE68b"
+      const contractABI = abi.abi
+
+      if (ethereum) {
+        const provider = new ethers.providers.Web3Provider(ethereum);
+        const signer = provider.getSigner();
+        const wavePortalContract = new ethers.Contract(contractAddress, contractABI, signer);
+
+        const waveTxn = await wavePortalContract.wave();
+        console.log("Mining...", waveTxn.hash);
+
+        await waveTxn.wait();
+        console.log("Mined -- ", waveTxn.hash);
+
+        let count = await wavePortalContract.getTotalWaves();
+        console.log("Retrieved total wave count...", count.toNumber());
+      } else {
+        console.log("Ethereum object doesn't exist!");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  const [currentAccount, setCurrentAccount] = React.useState("");
+  const checkIfWalletIsConnected = async () => {
+    try{
+      const { ethereum } = window;
+
+      if (!ethereum) {
+        console.log("Make sure you have metamask!");
+      } else {
+        console.log("We have the ethereum object", ethereum);
+      }
+
+      const accounts = await ethereum.request({ method: "eth_accounts" });
+
+        if (accounts.length !== 0) {
+          const account = accounts[0];
+          console.log("Found an authorized account:", account);
+          setCurrentAccount(account)
+        } else {
+          console.log("No authorized account found")
+        }
+      }catch(e){
+        console.log(e);
+      }
     
   }
+
+  const connectWallet = async () => {
+    try {
+      const { ethereum } = window;
+
+      if (!ethereum) {
+        alert("Get MetaMask!");
+        return;
+      }
+
+      const accounts = await ethereum.request({ method: "eth_requestAccounts" });
+
+      console.log("Connected", accounts[0]);
+      setCurrentAccount(accounts[0]);
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  React.useEffect(()=>{
+    checkIfWalletIsConnected();
+  },[])
   
   return (
     <div className="mainContainer">
 
       <div className="dataContainer">
         <div className="header">
-        👋 Hey there!
+        Hello there! 👋👋
         </div>
 
         <div className="bio">
-        I am Mihir and I worked on blockchain DApps, so that's pretty cool right? Connect your Ethereum wallet and wave at me!
+        I am Mihir and I worked on blockchain DApps! Connect your Ethereum wallet and wave at me 😃!
         </div>
 
         <button className="waveButton" onClick={wave}>
           Wave at Me
         </button>
+        {!currentAccount && (
+          <button className="waveButton" onClick={connectWallet}>
+            Connect Wallet
+          </button>
+        )}
       </div>
     </div>
   );
